12b8fa57ed17372b2f76718360e7838693d35cb4
QismetEhedov50 <114685429+QismetEhedov50@users.noreply.github.com>
2023-01-12 17:07:29 +0400

    Lesson28

---
+++ Lesson28/home.js
@@ -0,0 +1,7 @@
+var front=prompt("Please enter a number");
+for(var x=0; x<=100; x++){
+var front=document.createElement("div");
+front.style.width="150px";
+front.style.height="150px";
+document.getElementById("myDIV").appendChild(front);
+}
