e83427f003c30807a10939b4212a32d152299763
QismetEhedov50 <114685429+QismetEhedov50@users.noreply.github.com>
2023-01-30 18:19:17 +0400

    Lesson32

---
+++ Lesson32/main.js
@@ -0,0 +1,49 @@
+// function Multiplication(number){
+//     for(var i=1; i<=10; i++){
+//         document.write(`${number} * ${i} = ${i*number} <br>`)
+//     }
+// }
+// Multiplication(1)
+// Multiplication(2)
+// Multiplication(3)
+// Multiplication(4)
+// Multiplication(5)
+// Multiplication(6)
+// Multiplication(7)
+// Multiplication(8)
+// Multiplication(9)
+// Multiplication(10)
+
+// var netice=""
+// function Palindrome(word){
+//     for(var i=word.length-1; i>=0; --i ){
+//         netice+=word[i]
+//     }
+//     if(word=netice){
+//         console.log(netice  + " it is palindrome")
+//     }
+//     else{
+//         console.log("it is not palindrome")
+//     }
+// }
+// Palindrome("madam")
+
+//  function deyisen(number){
+//     for(var i=1; i<=1; i++){
+//         document.write(`${number**2} + ${i} = ${i+number**2} <br>`)
+//      }
+// }
+// deyisen(66)
+// deyisen(10)
+// deyisen(37)
+
+// function fib(n){
+//     const result=[0,100];
+//     for(var i=2;  i <= n; i++){
+//         var a=result[i-1];
+//         var b=result[i-2];
+//         result.push(a+b);
+//     }
+//     return result[n];
+// }
+// console.log(fib(1))
