195c2733f96ac0f206d19e3a087f5dfa78d4fa7e
QismetEhedov50 <114685429+QismetEhedov50@users.noreply.github.com>
2023-03-04 14:20:18 +0400

    Lesson41

---
+++ Lesson41/main.js
@@ -0,0 +1,136 @@
+//1
+var frontclick1 = document.querySelector(".front1");
+frontclick1.addEventListener("click", function () {
+  document.getElementById("two").classList.add("d-none")
+  document.getElementById("three").classList.add("d-none")
+  document.getElementById("four").classList.add("d-none")
+  document.getElementById("five").classList.add("d-none")
+  document.getElementById("six").classList.add("d-none")
+  document.getElementById("seven").classList.add("d-none")
+  document.getElementById("eight").classList.add("d-none")
+  document.getElementById("first").classList.add("d-none")
+  document.getElementById("one").classList.remove("d-none")
+  one.style.color = "white"
+});
+
+var frontclick2 = document.querySelector(".front2");
+frontclick2.addEventListener("click", function () {
+  document.getElementById("one").classList.add("d-none")
+  document.getElementById("three").classList.add("d-none")
+  document.getElementById("four").classList.add("d-none")
+  document.getElementById("five").classList.add("d-none")
+  document.getElementById("six").classList.add("d-none")
+  document.getElementById("seven").classList.add("d-none")
+  document.getElementById("eight").classList.add("d-none")
+  document.getElementById("first").classList.add("d-none")
+  document.getElementById("two").classList.remove("d-none")
+  two.style.color = "white"
+});
+
+
+var frontclick3 = document.querySelector(".front3");
+frontclick3.addEventListener("click", function () {
+  document.getElementById("one").classList.add("d-none")
+  document.getElementById("two").classList.add("d-none")
+  document.getElementById("four").classList.add("d-none")
+  document.getElementById("five").classList.add("d-none")
+  document.getElementById("six").classList.add("d-none")
+  document.getElementById("seven").classList.add("d-none")
+  document.getElementById("eight").classList.add("d-none")
+  document.getElementById("first").classList.add("d-none")
+  document.getElementById("three").classList.remove("d-none")
+  three.style.color = "white"
+});
+
+var frontclick4 = document.querySelector(".front4");
+frontclick4.addEventListener("click", function () {
+  document.getElementById("three").classList.add("d-none")
+  document.getElementById("one").classList.add("d-none")
+  document.getElementById("two").classList.add("d-none")
+  document.getElementById("five").classList.add("d-none")
+  document.getElementById("six").classList.add("d-none")
+  document.getElementById("seven").classList.add("d-none")
+  document.getElementById("eight").classList.add("d-none")
+  document.getElementById("first").classList.add("d-none")
+  document.getElementById("four").classList.remove("d-none")
+  four.style.color = "white"
+});
+
+var frontclick5 = document.querySelector(".front5");
+frontclick5.addEventListener("click", function () {
+  document.getElementById("one").classList.add("d-none")
+  document.getElementById("two").classList.add("d-none")
+  document.getElementById("three").classList.add("d-none")
+  document.getElementById("four").classList.add("d-none")
+  document.getElementById("six").classList.add("d-none")
+  document.getElementById("seven").classList.add("d-none")
+  document.getElementById("eight").classList.add("d-none")
+  document.getElementById("first").classList.add("d-none")
+  document.getElementById("five").classList.remove("d-none")
+  five.style.color = "white"
+});
+
+var frontclick6 = document.querySelector(".front6");
+frontclick6.addEventListener("click", function () {
+  document.getElementById("one").classList.add("d-none")
+  document.getElementById("two").classList.add("d-none")
+  document.getElementById("three").classList.add("d-none")
+  document.getElementById("four").classList.add("d-none")
+  document.getElementById("five").classList.add("d-none")
+  document.getElementById("seven").classList.add("d-none")
+  document.getElementById("eight").classList.add("d-none")
+  document.getElementById("first").classList.add("d-none")
+  document.getElementById("six").classList.remove("d-none")
+  six.style.color = "white"
+});
+
+var frontclick7 = document.querySelector(".front7");
+frontclick7.addEventListener("click", function () {
+  document.getElementById("one").classList.add("d-none")
+  document.getElementById("two").classList.add("d-none")
+  document.getElementById("three").classList.add("d-none")
+  document.getElementById("four").classList.add("d-none")
+  document.getElementById("five").classList.add("d-none")
+  document.getElementById("six").classList.add("d-none")
+  document.getElementById("eight").classList.add("d-none")
+  document.getElementById("first").classList.add("d-none")
+  document.getElementById("seven").classList.remove("d-none")
+  seven.style.color = "white"
+});
+
+var frontclick8 = document.querySelector(".front8");
+frontclick8.addEventListener("click", function () {
+  document.getElementById("one").classList.add("d-none")
+  document.getElementById("two").classList.add("d-none")
+  document.getElementById("three").classList.add("d-none")
+  document.getElementById("four").classList.add("d-none")
+  document.getElementById("five").classList.add("d-none")
+  document.getElementById("six").classList.add("d-none")
+  document.getElementById("seven").classList.add("d-none")
+  document.getElementById("first").classList.add("d-none")
+  document.getElementById("eight").classList.remove("d-none")
+  eight.style.color = "white"
+});
+
+//2
+var BtnClick1 = document.querySelector(".Btnclick1");
+BtnClick1.addEventListener("click", function () {
+  document.getElementById("collapseTwo").classList.add("d-none")
+  document.getElementById("collapseThree").classList.add("d-none")
+  document.getElementById("collapseOne").classList.remove("d-none")
+})
+
+var BtnClick2 = document.querySelector(".Btnclick2");
+BtnClick2.addEventListener("click", function () {
+  document.getElementById("collapseOne").classList.add("d-none")
+  document.getElementById("collapseThree").classList.add("d-none")
+  document.getElementById("collapseTwo").classList.remove("d-none")
+})
+
+var BtnClick3 = document.querySelector(".Btnclick3");
+BtnClick3.addEventListener("click", function () {
+  document.getElementById("collapseOne").classList.add("d-none")
+  document.getElementById("collapseTwo").classList.add("d-none")
+  document.getElementById("collapseThree").classList.remove("d-none")
+})
+
